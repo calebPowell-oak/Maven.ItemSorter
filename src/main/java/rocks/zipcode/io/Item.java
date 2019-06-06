35fc5c2cfae70fda37fc05e909a589cc07f9b774
Caleb Powell <calebpowell@zipcoders-MacBook-Pro.local>
2019-06-06 16:15:07 -0400

    Completed class stubs Item, ItemSorter, the 3 comparator classes

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -4,30 +4,41 @@ package rocks.zipcode.io;
  * @author leon on 30/01/2019.
  */
 public class Item {
+    private String name;
+    private Double price;
+    private Long id;
+
     public Item() {
+        this(0l, "", 0.01);
     }
 
     public Item(Long id, String name, Double price) {
+        this.name = name;
+        this.price = price;
+        this.id = id;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
-    }
-
-    public Long getId() {
-        return null;
-    }
-
-    public void setId(Long id) {
+        this.name = name;
     }
 
     public Double getPrice() {
-        return null;
+        return price;
     }
 
     public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
